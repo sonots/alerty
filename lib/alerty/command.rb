f4521410002d038d78e718af9f03028aa85319fb
sonots <sonots@gmail.com>
2015-11-23 17:43:14 +0900

    Support retry

--- lib/alerty/command.rb
+++ lib/alerty/command.rb
@@ -11,41 +11,46 @@ class Alerty
     end
 
     def run!
-      started_at = Time.now
-      begin
-        result = Frontkick.exec("#{@command} 2>&1", @opts)
-      rescue Frontkick::Timeout => e
-        record = {
-          hostname:   @hostname,
-          command:    @command,
-          exitstatus: 1,
-          output:     "`#{@command}` is timeout (#{@opts[:timeout]} sec)",
-          started_at: started_at.to_f,
-          duration:   @opts[:timeout],
-        }
-      rescue Frontkick::Locked => e
-        record = {
-          hostname:   @hostname,
-          command:    @command,
-          exitstatus: 1,
-          output:     "`#{@opts[:exclusive]}` is locked by another process",
-          started_at: started_at.to_f,
-          duration:   0,
-        }
-      else
-        record = {
-          hostname:   @hostname,
-          command:    @command,
-          exitstatus: result.exitstatus,
-          output:     result.stdout,
-          started_at: started_at.to_f,
-          duration:   result.duration,
-        }
+      record = {}
+      with_retry do |retries|
+        started_at = Time.now
+        begin
+          result = Frontkick.exec("#{@command} 2>&1", @opts)
+        rescue Frontkick::Timeout => e
+          record = {
+            hostname:   @hostname,
+            command:    @command,
+            exitstatus: 1,
+            output:     "`#{@command}` is timeout (#{@opts[:timeout]} sec)",
+            started_at: started_at.to_f,
+            duration:   @opts[:timeout],
+            retries:    retries,
+          }
+        rescue Frontkick::Locked => e
+          record = {
+            hostname:   @hostname,
+            command:    @command,
+            exitstatus: 1,
+            output:     "`#{@opts[:exclusive]}` is locked by another process",
+            started_at: started_at.to_f,
+            duration:   0,
+            retries:    retries,
+          }
+        else
+          record = {
+            hostname:   @hostname,
+            command:    @command,
+            exitstatus: result.exitstatus,
+            output:     result.stdout,
+            started_at: started_at.to_f,
+            duration:   result.duration,
+            retries:    retries,
+          }
+        end
+        Alerty.logger.info { "result: #{record.to_json}" }
+        record
       end
-      Alerty.logger.info { "result: #{record.to_json}" }
-      if record[:exitstatus] == 0
-        exit 0
-      else
+      unless record[:exitstatus] == 0
         Config.plugins.each do |plugin|
           begin
             plugin.alert(record)
@@ -56,5 +61,19 @@ class Alerty
         exit record[:exitstatus]
       end
     end
+
+    private
+
+    def with_retry
+      retries = 0
+      while true
+        record = yield(retries)
+        break if record[:exitstatus] == 0
+        break if retries >= Config.retry_limit
+        retries += 1
+        sleep Config.retry_interval
+      end
+    end
+
   end
 end
